0201fb6fc9be4183707f99b8af82ac999a00d8be
MaksT1 <makstkaczyk01@gmail.com>
2026-03-07 13:06:25 +0100

    zad5

---
+++ src/zad5.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class zad5 {
+    public static void main(String[] args){
+        Scanner skan = new Scanner(System.in);
+        System.out.println("Podaj liczbe 1-12");
+        int mies = skan.nextInt();
+
+        switch(mies){
+            case 1:
+                System.out.println("Styczen: 31 dni"); break;
+            case 2:
+                System.out.println("Luty: 28 dni"); break;
+            case 3:
+                System.out.println("Marzec: 31 dni"); break;
+            case 4:
+                System.out.println("Kwiecien: 30 dni"); break;
+            case 5:
+                System.out.println("Maj: 31 dni"); break;
+            case 6:
+                System.out.println("Czerwiec: 30 dni"); break;
+            case 7:
+                System.out.println("Lipiec: 31 dni"); break;
+            case 8:
+                System.out.println("Sierpien: 31 dni"); break;
+            case 9:
+                System.out.println("Wrzesien: 30 dni"); break;
+            case 10:
+                System.out.println("Pazdziernik: 31 dni"); break;
+            case 11:
+                System.out.println("Listopad: 30 dni"); break;
+            case 12:
+                System.out.println("Grudzien: 31 dni"); break;
+            default:
+                System.out.println("Nie ma takiego miesiaca"); break;
+        }
+
+    }
+}
