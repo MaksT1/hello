4c2eacf7126691fc1d2aa31a3d406e0d33905c57
MaksT1 <makstkaczyk01@gmail.com>
2026-03-07 12:31:59 +0100

    zad1 i 2

---
+++ src/zad2.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.PI;
+import static java.lang.Math.sqrt;
+
+public class zad2 {
+    public static void main(String[] args){
+        double a = sqrt(PI);
+        System.out.printf("%1.2f", a);
+    }
+}
